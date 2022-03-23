ef5194a08f3df03edce7fc25d5a2998593de4d9e
marlencar <alencar.marcelo1997@gmail.com>
2022-03-23 14:59:06 -0300

    fixes

--- assets/javascript/products.js
+++ assets/javascript/products.js
@@ -2,48 +2,48 @@ export const products = [
     {
       name: "lorem ipsun",
       price: "99,90",
-      img: "./public/gabrielle-henderson-1DMNn6gBbwQ-unsplash.jpg"
+      img: "./assets/image/gabrielle-henderson-1DMNn6gBbwQ-unsplash.jpg"
     },
     
     {
       name: "lorem ipsun",
       price: "24,90",
-      img: "./public/devin-avery-bMH8ub9z1_U-unsplash.jpg",
+      img: "./assets/image/devin-avery-bMH8ub9z1_U-unsplash.jpg",
     },
      
     {
       name: "lorem ipsun",
       price: "79,90",
-      img: "./public/devin-avery-BRVqq2uak4E-unsplash.jpg",
+      img: "./assets/image/devin-avery-BRVqq2uak4E-unsplash.jpg",
     },
     {
       name: "lorem ipsun",
       price: "39,90",
-      img: "./public/nery-montenegro-SmEty_TVr80-unsplash.jpg",
+      img: "./assets/image/nery-montenegro-SmEty_TVr80-unsplash.jpg",
     },
     {
       name: "lorem ipsun",
       price: "249,90",
-      img: "./public/olena-sergienko-GOVTETevRm8-unsplash.jpg",
+      img: "./assets/image/olena-sergienko-GOVTETevRm8-unsplash.jpg",
     },
     {
       name: "lorem ipsun",
       price: "89,90",
-      img: "./public/pharma-hemp-complex-tAzdKblAinw-unsplash.jpg",
+      img: "./assets/image/pharma-hemp-complex-tAzdKblAinw-unsplash.jpg",
     },
     {
       name: "lorem ipsun",
       price: "59,90",
-      img: "./public/phaedra-botanicals-uFAHrKYkTVI-unsplash.jpg",
+      img: "./assets/image/phaedra-botanicals-uFAHrKYkTVI-unsplash.jpg",
     },
     {
       name: "lorem ipsun",
       price: "39,90",
-      img: "./public/pharma-hemp-complex-yVAXSK6zFIM-unsplash.jpg",
+      img: "./assets/image/pharma-hemp-complex-yVAXSK6zFIM-unsplash.jpg",
     },
     {
       name: "lorem ipsun",
       price: "34,90",
-      img: "./public/chandra-oh-y5hRv6UNKMg-unsplash.jpg",
+      img: "./assets/image/chandra-oh-y5hRv6UNKMg-unsplash.jpg",
     },
   ];
